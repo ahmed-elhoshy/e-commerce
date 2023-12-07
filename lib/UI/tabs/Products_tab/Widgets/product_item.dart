e81082fdff104f8f7b59c3a06d29d7aa24b555c3
ahmed-elhoshy <93227407+ahmed-elhoshy@users.noreply.github.com>
2023-12-07 03:35:29 +0200

    added products tab and managed apis and repositories

--- lib/UI/tabs/Products_tab/Widgets/product_item.dart
+++ lib/UI/tabs/Products_tab/Widgets/product_item.dart
@@ -1,5 +1,4 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:e_commerce/UI/tabs/Products_tab/Widgets/view_product_button.dart';
 import 'package:e_commerce/Utils/app_colors.dart';
 import 'package:flutter/material.dart';
 
@@ -16,53 +15,97 @@ class ProductItem extends StatelessWidget {
     return Center(
       child: Stack(children: [
         Container(
-          height: MediaQuery.of(context).size.height * 0.7,
           width: 180,
           decoration: BoxDecoration(
               border: Border.all(
                 color: AppColors.primaryColor,
               ),
               borderRadius: BorderRadius.circular(13)),
-          child: Column(
-            children: [
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.195,
-              ),
-              Text(productsEntity.title ?? "",
-                  style: TextStyle(
-                    color: AppColors.primaryColor,
-                    overflow: TextOverflow.ellipsis,
-                    fontSize: 13,
-                  )),
-              const SizedBox(
-                height: 5,
-              ),
-              Text(
-                "${productsEntity.price} EGP",
-                style: TextStyle(
-                  color: AppColors.primaryColor,
-                  fontSize: 13,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.185,
                 ),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              ViewProductButton(textOfButton: 'View Product', onPressed: () {}),
-            ],
+                Text(productsEntity.title ?? "",
+                    style: TextStyle(
+                      color: AppColors.black,
+                      overflow: TextOverflow.ellipsis,
+                      fontSize: 13,
+                    )),
+                Text(
+                  "EGP ${productsEntity.price}",
+                  style: TextStyle(
+                    color: AppColors.black,
+                    fontSize: 14,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      child: Row(
+                        children: [
+                          Text(
+                            "Review (${productsEntity.ratingsAverage})",
+                            style: TextStyle(
+                              color: AppColors.black,
+                              fontSize: 13,
+                            ),
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: Icon(
+                        Icons.add_circle,
+                        color: AppColors.primaryColor,
+                        size: 30,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
         Container(
-          height: MediaQuery.of(context).size.height * 0.19,
+          height: MediaQuery.of(context).size.height * 0.18,
           width: 180,
           decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(14), topLeft: Radius.circular(14)),
             border: Border.all(color: AppColors.primaryColor, width: 1),
           ),
-          child: CachedNetworkImage(
-            fit: BoxFit.cover,
-            imageUrl: productsEntity.imageCover ?? "",
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(14),
+            child: CachedNetworkImage(
+              fit: BoxFit.cover,
+              imageUrl: productsEntity.imageCover ?? "",
+            ),
           ),
         ),
+        Padding(
+          padding: EdgeInsets.only(
+              left: MediaQuery.of(context).size.width * 0.35,
+              top: MediaQuery.of(context).size.height * 0.01),
+          child: Container(
+              height: 35,
+              width: 35,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50), color: Colors.white),
+              child: Icon(
+                Icons.favorite_border,
+                color: AppColors.primaryColor,
+              )),
+        )
       ]),
     );
   }
--- lib/UI/tabs/Products_tab/product_list_tab.dart
+++ lib/UI/tabs/Products_tab/product_list_tab.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce/UI/tabs/Products_tab/Cubit/product_list_states.dart';
 import 'package:e_commerce/UI/tabs/Products_tab/Cubit/product_list_view_model.dart';
+import 'package:e_commerce/Utils/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -18,6 +19,48 @@ class ProductListTab extends StatelessWidget {
           builder: (context, state) {
             return Column(
               children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                        height: MediaQuery.of(context).size.height * 0.07,
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(26),
+                          color: Theme.of(context).colorScheme.surface,
+                          border: (Border.all(
+                              color: AppColors.primaryColor, width: 1)),
+                        ),
+                        child: Row(
+                          children: [
+                            IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.search,
+                                  color: AppColors.primaryColor,
+                                  size: 30,
+                                )),
+                            Text(
+                              'what do you search for ?',
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                          ],
+                        )),
+                    IconButton(
+                        onPressed: () {
+                          // Navigator.of(context)
+                          //     .pushNamed(AddToCart.routename);
+                        },
+                        icon: Icon(
+                          Icons.shopping_cart_outlined,
+                          color: AppColors.primaryColor,
+                          size: 30,
+                        )),
+                  ],
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.04,
+                ),
                 state is ProductLoadingState
                     ? Center(child: CircularProgressIndicator())
                     : Expanded(
@@ -26,7 +69,7 @@ class ProductListTab extends StatelessWidget {
                                 const SliverGridDelegateWithFixedCrossAxisCount(
                                     crossAxisCount: 2,
                                     mainAxisSpacing: 25,
-                                    childAspectRatio: 1 / 1.30),
+                                    childAspectRatio: 1 / 1.1),
                             itemCount: viewModel.productsList.length,
                             itemBuilder: (BuildContext context, int index) {
                               return InkWell(
